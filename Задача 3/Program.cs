308f882223d35eae0d29ca15a8ce7faa175f1354
Лазарев Константин Михайлович <kmlazarev@edu.hse.ru>
2020-06-15 01:38:35 +0500

    Добавьте файлы проекта.

---
+++ Задача 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            bool ok = false;
+            double f(double x) => Math.Abs(Math.Abs(x) - 1); // функция для графика
+            while (!ok)
+            {
+                Console.WriteLine("Введите число, для которого необходимо вычислить функцию");
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    ok = true;
+                }
+                catch { Console.WriteLine("Ошибка ввода"); }
+            }
+
+            Console.WriteLine("\nПолученное значение функции: " + f(a));
+            Console.ReadKey();
+        }
+    }
+}
